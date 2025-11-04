8d76cb2d09c82048a22f576610efd22816cc11a4
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-11-04 18:34:46 +0100

    completati i primi 3 esercizi

--- esercizi_classe_array/Program.cs
+++ esercizi_classe_array/Program.cs
@@ -12,7 +12,36 @@
             {
                 Console.Write("[" + numContr[i] + "]");
             }
+            
+            Console.WriteLine();
 
+            //2
+
+            int[] indP = { 4, 7, 8, 2, 9 };
+
+            for (int i = 0; i < indP.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("[" + indP[i] + "]");
+                }
+               
+            }
+
+            Console.WriteLine();
+
+            //3
+
+            int[] numD = { 4, 7, 8, 2, 9 };
+
+            for (int i = 0; i < numD.Length; i++)
+            {
+                if (numD[i] % 2 == 1)
+                {
+                    Console.Write("[" + numD[i] + "]");
+                }
+
+            }
 
         }
     }
