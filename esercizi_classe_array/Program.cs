8becadbf4e32852eb5811c4ac14f38ac3e32ca60
conti.davide <conti.davide@HP-005>
2025-11-04 10:58:58 +0100

    Aggiungere i file di progetto.

---
+++ esercizi_classe_array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace esercizi_classe_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+
+            int[] numContr = {4, 7, 8, 2, 9};
+
+            for (int i = numContr.Length-1; i >= 0; i--)
+            {
+                Console.Write("[" + numContr[i] + "]");
+            }
+
+
+        }
+    }
+}
